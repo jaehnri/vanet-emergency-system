c2446b743c50aeb44cfb4862f48ad1dd889c3fac
jaehnri <joao.henri.cr@gmail.com>
2022-10-11 00:15:07 -0300

    Make ERU send message to nearest hospital
    Signed-off-by: jaehnri <joao.henri.cr@gmail.com>

--- src/veins/modules/application/traci/ERUApp.h
+++ src/veins/modules/application/traci/ERUApp.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "veins/modules/application/ieee80211p/DemoBaseApplLayer.h"
+#include "veins/modules/application/traci/HospitalInfo.h"
+
 using namespace omnetpp;
 namespace veins {
 
@@ -7,20 +9,27 @@ class VEINS_API ERUApp : public DemoBaseApplLayer {
 public:
     void initialize(int stage) override;
     void finish() override;
+    HospitalInfo firstHospital;
+    HospitalInfo secondHospital;
 
 protected:
     //Additions
     void handleMessage(cMessage *msg) override;
     int rsuIn;
     int rsuOut;
-    int hospitalIn;
-    int hospitalOut;
+    int firstHospitalIn;
+    int firstHospitalOut;
+    int secondHospitalIn;
+    int secondHospitalOut;
+
     //void onBSM(DemoSafetyMessage* bsm) override;
     void onWSM(BaseFrame1609_4* wsm) override;
     //void onWSA(DemoServiceAdvertisment* wsa) override;
 
     void handleSelfMsg(cMessage* msg) override;
     //void handlePositionUpdate(cObject* obj) override;
+
+    int getNearestHospital(Coord accidentLocation, HospitalInfo first, HospitalInfo second);
 };
 
 } // namespace veins
---
+++ src/veins/modules/application/traci/HospitalInfo.h
@@ -0,0 +1,14 @@
+//
+// Created by joao on 10/10/22.
+//
+
+#ifndef VANET_EMERGENCY_SYSTEM_HOSPITALLOCATIONS_H
+#define VANET_EMERGENCY_SYSTEM_HOSPITALLOCATIONS_H
+
+class HospitalInfo{
+public:
+    double x;
+    double y;
+};
+
+#endif // VANET_EMERGENCY_SYSTEM_HOSPITALLOCATIONS_H
