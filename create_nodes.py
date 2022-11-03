852b6f336339d40c5c16175d23200909154a89a3
brunopecampos <brunopecampos@protonmail.com>
2022-11-03 20:15:42 -0300

    Add highway drawing to simulation

--- create_nodes.py
+++ create_nodes.py
@@ -2,8 +2,8 @@ f = open("./examples/veins/first.nod.xml", 'w')
 f.write("<nodes>\n")
 for i in range(6):
   for j in range(6):
-    x = i * 500.00
-    y = j * 500.00
+    x = i * 300.00
+    y = j * 300.00
     if i == 3 and j == 3:
       f.write(f'\t<node id="n{i}{j}" x="{x}" y="{y}" type="traffic_light" />\n')
     else:
