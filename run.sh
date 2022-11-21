6d1b79f5a9670ef9e90a9ee430a48c22c0945200
brunopecampos <brunopecampos@protonmail.com>
2022-11-21 14:36:55 -0300

    Add script to run simulation without IDE (run.sh)

---
+++ run.sh
@@ -0,0 +1,4 @@
+#/bin/sh
+cd /home/brunopec/Downloads/Omnet-5.6.2/vanet-emergency-system/examples/veins
+opp_run -m -u Qtenv -n .:../../src/veins --image-path=../../images -l ../../src/veins omnetpp.ini
+#opp_run -m -u Qtenv -n .:../../src/veins --image-path=../../images -l ../../src/veins omnetpp.ini
